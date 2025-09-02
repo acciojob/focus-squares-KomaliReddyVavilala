4f33263c766f50e8fc3d93c2e6c320e7d639faf4
KomaliReddyVavilala <161595708+KomaliReddyVavilala@users.noreply.github.com>
2025-09-02 16:57:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+// const boxes = document.querySelectorAll(".sqaure");
+// for(let box of boxes){
+// 	box.addEventListener("mouseover",(e)=>addBgColor(e));
+// 	box.addEventListener("mouseout",(e)=>addDefaultBgColor(e));
+// }
+// function addBgColor(e) {
+//       const selectedBoxId = e.target.id;
+//       for (let b of boxes) {
+//         if (b.id !== selectedBoxId) {
+//           b.style.backgroundColor = "#6F4E37"; // Coffee for others
+//         } else {
+//           b.style.backgroundColor = "#E6E6FA"; // Keep hovered Lavender
+//         }
+//     }
+// }
+// function addDefaultBgColor() {
+// 	for(let box of boxes){
+// 		box.style.backgroundColor = "#E6E6FA"
+// 	}
+	
+// }
